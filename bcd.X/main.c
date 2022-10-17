d7ca6ceb2ce3f439b4109d700b0c03ce516edf63
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2022-10-17 07:04:18 -0500

    pic16f877a examples

---
+++ bcd.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: orlan
+ *
+ * Created on 11 de octubre de 2022, 13:38
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ pulse.X/main.c
@@ -0,0 +1,29 @@
+/*
+ * File:   main.c
+ * Author: orlan
+ *
+ * Created on 11 de octubre de 2022, 18:18
+ */
+
+#include "configs.h"
+
+void main(void) {
+    TRISB0 = 0x0;
+    TRISA4 = 0x1;
+
+    PORTBbits.RB0 = 0;
+    PORTAbits.RA4 = 0;
+
+    while (1) {
+        if (PORTAbits.RA4 == 0) {
+            __delay_ms(50);
+            if (PORTAbits.RA4 == 0) {
+                PORTBbits.RB0 = 1;
+                __delay_ms(5);
+                PORTBbits.RB0 = 0;
+                __delay_ms(5000);
+            }
+        }
+        
+    }
+}
